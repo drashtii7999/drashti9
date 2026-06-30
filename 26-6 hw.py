5bed137f67bfdadb2f205b1bf613fd358b1054cc
Drashti Patel <pateldrashti7999@gmail.com>
2026-07-01 01:31:06 +0530

    26 - 6 hw

---
+++ 26-6 hw.py
@@ -0,0 +1,20 @@
+#Smoothie Maker
+fruit1 = input("Choose first fruit: ")
+fruit2 = input("Choose second fruit: ")
+milk = input("Add milk (Yes/No): ")
+
+print("\nMaking your smoothie...")
+
+print("ingredents:")
+print("-", fruit1)
+print("-", fruit2)
+
+if milk == "Yes":
+    print("-milk")
+
+print("Your delicious smoothie is ready!")
+
+
+
+
+
