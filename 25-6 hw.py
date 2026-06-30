5e256f09da5b5bc301fa86f67127bc6ff1b54e57
Drashti Patel <pateldrashti7999@gmail.com>
2026-07-01 00:55:48 +0530

    25-6 hw

---
+++ 25-6 hw.py
@@ -0,0 +1,34 @@
+#Magic Juice Maker
+
+fruit =input("Choose your fruit (mango / apple / orange / grape):")
+sugar = input("Add sugar?(Yes/No):")
+ice = input("Add ice?(Yes/No):")
+
+print("Making your magic juice...")
+print("Blending",fruit,"with love and magic")
+print("Your",fruit, "juice is ready! Enjoy your drink")
+
+#Age category pattern
+age = int(input("Enter your age: "))
+if age < 13:
+    print("Child")
+elif age < 20:
+    print("Teenager")
+else:
+    print("Adult")
+
+
+#pizza store
+
+print("Welcome to Pizza shop!")
+size = input("Choose size(Small / Medium / Large):")
+topping = input("Choose topping (Cheese /Corn / Paneer):")
+drink = input("Choose drink (Coke / Pepsi / Water):")
+
+print("\nYour order is ready!")
+print(size, "pizza with",topping)
+print("Drink:",drink)
+print("Enjoy your meal!")
+print(" Git Test")
+
+    
