c364a12b2ad3c879f6c0b3aab43ec97de0a4e37a
Drashti Patel <pateldrashti7999@gmail.com>
2026-07-01 01:35:32 +0530

    task 1,2

---
+++ task.py
@@ -0,0 +1,26 @@
+#name age & city
+
+name = "drashti"
+age = 17
+city = "surat"
+
+# Welcome user
+
+print(f"name: {name}\nage: {age}\ncity {city}")
+
+name = input("Enter your name: ")
+print(f"Hello, {name}!Welcome to Python!")
+
+#variable
+
+a = 10
+b = 2.5
+c = "Hello"
+d = True
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+
